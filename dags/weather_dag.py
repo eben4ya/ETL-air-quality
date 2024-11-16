c7eb1924b854a5d7ad42a51c5e3270fa27a8c51a
Ubuntu <benjakmek@WeatherAirQualityAPI.q3k2uajbaveubm2xuzstq5akag.hx.internal.cloudapp.net>
2024-11-16 10:31:52 +0000

    <feat: initialize project>

---
+++ dags/weather_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 11, 16),
+    'email': ['dzakiwismadi@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG('weather_dag',
+         default_args=default_args,
+         description='A simple weather DAG',
+         schedule_interval='@daily',
+         catchup=False) as dag:
+  
+    is_weather_api_ready = HttpSensor(
+        task_id='is_weather_api_ready',
+        http_conn_id='weathermap_api',
+        endpoint='/data/2.5/weather?q=Jakarta&appid=a394145349a7323a58c762a69910fdfd',
+    )
